54792a67a1d08f36e3177b0c5498083d82ff16c1
Patrick Thomson <patrickt@github.com>
2022-02-13 12:42:58 -0500

    Imitate ispell's command-line

--- enchant/main.swift
+++ enchant/main.swift
@@ -6,6 +6,31 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+let spellChecker = NSSpellChecker.shared
 
+while true {
+    print("word: ", terminator: "")
+    if let input = readLine(strippingNewline: true) {
+        if !input.isEmpty {
+            let found = spellChecker.checkSpelling(of: input, startingAt: 0, language: nil, wrap: false, inSpellDocumentWithTag: 0, wordCount: nil)
+            if found.location != NSNotFound {
+                if let guesses = spellChecker.guesses(forWordRange: found, in: input, language: nil, inSpellDocumentWithTag: 0) {
+                    if guesses.isEmpty {
+                        print("not found")
+                    } else {
+                        print("how about:", guesses.joined(separator: ", "))
+                    }
+                } else {
+                    print("not found")
+                }
+            } else {
+                print("ok")
+            }
+            print("")
+        }
+    } else {
+        break
+    }
+}
